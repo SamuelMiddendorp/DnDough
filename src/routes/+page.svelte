78d0009acdfbe3545c76ccd3463d3d24fcd347d5
Samuel Middendorp <samiezaurus@gmail.com>
2023-04-19 21:17:19 +0200

    setup svelte, initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<body>
+    <main>
+        <h1>DoughnD</h1>
+        <div class="die">
+            d20;
+        </div>
+    </main>
+</body>
+
+<style>
+    :root {
+        --statement-color: #3d816c;
+        --background-color: #060D0B;
+        --panel-color: #0C1A15;
+    }
+    @import url("https://fonts.googleapis.com/css2?family=Grenze+Gotisch&display=swap");
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+    }
+    main {
+        background-color: var(--background-color);
+        min-height: 100vh;
+        font-family: "Grenze Gotisch", cursive;
+    }
+</style>
