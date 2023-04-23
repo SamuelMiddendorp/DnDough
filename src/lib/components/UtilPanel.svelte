ec36ee7c663b45dd9798f26a27e774c146ed1473
Samuel Middendorp <samiezaurus@gmail.com>
2023-04-23 12:55:58 +0200

    Beginning of util panel

---
+++ src/lib/components/UtilPanel.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+let audio: boolean = false;
+let lightTheme: boolean = false;
+</script>
+<div class="util-panel shadow">
+    <p>Sound {audio ? "on" : "off"}</p>
+    <p>Theme: {lightTheme ? "light" : "dark"}</p>
+</div>
